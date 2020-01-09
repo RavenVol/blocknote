41e9827b964a5af2623e1bef22ca92e4fd177d48
RavenVol <r.volianiuk@gmail.com>
2020-01-09 16:33:06 +0200

    Updates

---
+++ precache-manifest.7e08648c2f61a926663d50c9c08497bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ded31f2b4a6c776d0d7f2907bff3e5",
+    "url": "/blocknote/index.html"
+  },
+  {
+    "revision": "1a7a300c6e7e4722b881",
+    "url": "/blocknote/static/css/main.702068e1.chunk.css"
+  },
+  {
+    "revision": "e51cceaea5a7de887374",
+    "url": "/blocknote/static/js/2.f94512c4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/blocknote/static/js/2.f94512c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a7a300c6e7e4722b881",
+    "url": "/blocknote/static/js/main.79b07ac1.chunk.js"
+  },
+  {
+    "revision": "e3948519fd525a73b39b",
+    "url": "/blocknote/static/js/runtime-main.b19f230e.js"
+  }
+]);
