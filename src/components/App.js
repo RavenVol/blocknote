142dd2ff7b220dac4ac7a63748f9df5bacd14e29
RavenVol <r.volianiuk@gmail.com>
2020-01-09 16:06:57 +0200

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import '../styles/css/reset.css';
+import '../styles/css/app.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      
+    }
+  }
+
+  render() {
+
+    return (
+      <>
+        <header className="header">
+
+        </header>
+
+        <main className="main">
+
+        </main>
+
+        <footer className="footer">
+
+        </footer>
+      </>
+    );
+  }
+}
+
+export default App;
